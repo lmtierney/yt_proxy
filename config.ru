cadeb6f073cdaf0ccdadc1534c39a982cf039eb8
Lucas Tierney <lucast1533@gmail.com>
2016-10-28 15:44:39 +0000

    make it run

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './proxy'
+run ProxyApp
--- proxy.rb
+++ proxy.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'rest-client'
 
-get '/youtube/*' do |url|
-  RestClient.get("https://www.googleapis.com/youtube/#{url}", params: params)
+class ProxyApp < Sinatra::Base
+  set :bind, '0.0.0.0'
+  set :port, 80
+  get '/youtube/*' do |url|
+    RestClient.get("https://www.googleapis.com/youtube/#{url}", params: params)
+  end
 end
